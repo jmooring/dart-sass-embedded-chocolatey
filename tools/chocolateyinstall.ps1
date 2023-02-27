67b329f89cdc3737a5428f1d43ba5a74079c85d8
Joe Mooring <joe.mooring@veriphor.com>
2023-02-26 18:56:02 -0800

    Update 1.56.1 => 1.58.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/sass/dart-sass-embedded/releases/download/1.56.1/sass_embedded-1.56.1-windows-ia32.zip'
-$url64      = 'https://github.com/sass/dart-sass-embedded/releases/download/1.56.1/sass_embedded-1.56.1-windows-x64.zip'
+$url        = 'https://github.com/sass/dart-sass-embedded/releases/download/1.58.3/sass_embedded-1.58.3-windows-ia32.zip'
+$url64      = 'https://github.com/sass/dart-sass-embedded/releases/download/1.58.3/sass_embedded-1.58.3-windows-x64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   url64bit      = $url64
   softwareName  = 'dart-sass-embedded*'
 
-  checksum      = '35910C75A091DA52E72D251438BA88142126ADEEBFE35AC64B617A24505083DA'
+  checksum      = '922CE0B17970A88A9A993047CE3CB9E3B82DD50558D4ADD62D1033E149263D93'
   checksumType  = 'sha256'
-  checksum64    = 'C8BF9C009AFD6BBEB1742E010845B84E1021FDBA6EDC80B4DD1FBBEE75354B39'
+  checksum64    = '5D34BAEA134D8693E27E2E98E098D48706B345A5F7FB145A97F06A3493B2AD79'
   checksumType64= 'sha256'
 }
 
